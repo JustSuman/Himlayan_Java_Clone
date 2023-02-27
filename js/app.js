b007b7e5858d2088659b354321cd6d0baa3c7f96
JustSuman <np03cs4a220062@heraldcollege.edu.np>
2023-02-27 22:02:28 +0545

    responsive modified

--- js/app.js
+++ js/app.js
@@ -2,7 +2,7 @@ gsap.registerPlugin(ScrollTrigger);
 
 gsap.from(".items", {
     duration: 1,
-    x:-100,
+    // x:-100,
     opacity:0, 
     stagger: 0.5,
     scrollTrigger:{
@@ -10,3 +10,9 @@ gsap.from(".items", {
         toggleActions: "play pause resume reset",
     }
 });
+
+// toggle menu bar
+function toggle(){
+    let a = document.querySelector(".menu");
+    a.style.display = ""
+}
